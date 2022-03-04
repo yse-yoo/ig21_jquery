4ce6a63fbc50ce4e2a16fdd350fba8cbf17cd408
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-03-04 10:46:41 +0900

    animation 上下左右

--- animation/animation.js
+++ animation/animation.js
@@ -2,23 +2,24 @@ var monster = $('#monster')
 console.log(monster)
 
 function show() {
-    monster.fadeIn()
+    monster.fadeIn(1000)
 }
 
 function hide() {
-    monster.fadeOut()
+    monster.fadeIn(1000)
 }
 
 function moveRight() {
+    monster.animate({left: 300}, 300)
 }
-
 function moveLeft() {
+    monster.animate({left: 0}, 300)
 }
-
 function moveUp() {
+    monster.animate({top: 50}, 300)
 }
-
 function moveDown() {
+    monster.animate({top: 300}, 300)
 }
 
 function moveSequence() {
