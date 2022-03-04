2c16e2d5eb7c972d56ca1beb7310ea196da2895d
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-03-04 10:22:32 +0900

    animation 追加

---
+++ animation/animation.js
@@ -0,0 +1,21 @@
+var monster = $('#monster')
+function show() {
+}
+
+function hide() {
+}
+
+function moveRight() {
+}
+
+function moveLeft() {
+}
+
+function moveUp() {
+}
+
+function moveDown() {
+}
+
+function moveSequence() {
+}
