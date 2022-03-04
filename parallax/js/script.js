35dd1d3c83b203cd45757c405c013da575508590
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-03-04 11:54:23 +0900

    window scroll 作成

--- parallax/js/script.js
+++ parallax/js/script.js
@@ -3,7 +3,6 @@ $(function () {
     $('a').on('click', function (event) {
         //$(this) はイベントの元、つまりクリックした aリンク
         //番号取得して、1引く
-        // var no = $(this).html() - 1
         var no = $(this).data('no') - 1
 
         //body に background-position を設定
@@ -11,11 +10,11 @@ $(function () {
         $('body').css('background-position', body_position)
 
         //#bg1 に background-position を設定
-        var bg1_position = no * -80 + 'px 100%'
+        var bg1_position = no * -180 + 'px 100%'
         $('#bg1').css('background-position', bg1_position)
 
         //#bg2 に background-position を設定
-        var bg2_position = no * -160 + 'px 100%'
+        var bg2_position = no * -460 + 'px 100%'
         $('#bg2').css('background-position', bg2_position)
 
         //文章のスライド
